3dca324494aa97d5351ba04e05e78313172c110c
Hamlet <hamlets849@gmail.com>
2024-07-08 11:25:04 +0400

    правки

--- telebot_wrapper/bot.py
+++ telebot_wrapper/bot.py
@@ -10,7 +10,7 @@ class Bot(TeleBot):
     def __init__(self, token: str, *args, **kwargs):
         super().__init__(token, *args, **kwargs)
 
-    def callback_query_handler(self, *args, **kwargs):
+    def callback_query_handler(self, *filters: Callable[[CallbackQuery], bool], **kwargs):
         def decorator(func: Callable[[CallbackQuery, list[str]], None]):
             @wraps(func)
             def wrapper(call: CallbackQuery):
@@ -18,7 +18,7 @@ class Bot(TeleBot):
                 data_list = call.data.split(";")
                 return func(call, data_list)
 
-            handler = super(Bot, self).callback_query_handler(*args, **kwargs)(wrapper)
+            handler = super(Bot, self).callback_query_handler(*filters, **kwargs)(wrapper)
             return handler
 
         return decorator
